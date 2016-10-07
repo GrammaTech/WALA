dcb38a29401ce4548edb77d4b911f52f632dc106
Lucja Kot <lkot@grammatech.com>
2016-10-07 08:37:28 -0400

    Add support for inclusions to override exclusions

--- com.ibm.wala.core.tests/src/com/ibm/wala/core/tests/cha/ExclusionsTest.java
+++ com.ibm.wala.core.tests/src/com/ibm/wala/core/tests/cha/ExclusionsTest.java
@@ -10,19 +10,25 @@
  *******************************************************************************/
 package com.ibm.wala.core.tests.cha;
 
-import java.io.IOException;
-
-import junit.framework.Assert;
-
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 import com.ibm.wala.core.tests.util.TestConstants;
 import com.ibm.wala.ipa.callgraph.AnalysisScope;
 import com.ibm.wala.types.ClassLoaderReference;
 import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.config.AnalysisScopeReader;
+import com.ibm.wala.util.config.ExplicitSetOfClasses;
+import com.ibm.wala.util.config.FileOfClasses;
+import com.ibm.wala.util.config.SetOfSetsOfClasses;
 import com.ibm.wala.util.io.FileProvider;
 import com.ibm.wala.util.strings.StringStuff;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashSet;
+import junit.framework.Assert;
+import org.junit.Test;
 
 public class ExclusionsTest {
 
@@ -34,4 +40,122 @@ public class ExclusionsTest {
         StringStuff.deployment2CanonicalTypeString("java.awt.Button"));
     Assert.assertTrue(scope.getExclusions().contains(buttonRef.getName().toString().substring(1)));
   }
+    @Test
+    public void testInclusionsOnly() throws IOException {
+        AnalysisScope scope =
+                AnalysisScopeReader.readJavaScope(
+                        TestConstants.WALA_TESTDATA, null, ExclusionsTest.class.getClassLoader());
+        TypeReference buttonRef =
+                TypeReference.findOrCreate(
+                        ClassLoaderReference.Application,
+                        StringStuff.deployment2CanonicalTypeString("java.awt.Button"));
+
+        SetOfSetsOfClasses exclusions = new SetOfSetsOfClasses(SetOfSetsOfClasses.Kind.INCL_ONLY);
+        Collection<String> inclusions = new HashSet<String>();
+        inclusions.add("java/awt/Button");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions, true, false));
+        scope.setExclusions(exclusions);
+
+        assertFalse(scope.getExclusions().contains(buttonRef.getName().toString().substring(1)));
+    }
+
+    @Test
+    public void testInclusionsOverrideExclusions() throws IOException {
+        AnalysisScope scope =
+                AnalysisScopeReader.readJavaScope(
+                        TestConstants.WALA_TESTDATA, null, ExclusionsTest.class.getClassLoader());
+        TypeReference buttonRef =
+                TypeReference.findOrCreate(
+                        ClassLoaderReference.Application,
+                        StringStuff.deployment2CanonicalTypeString("java.awt.Button"));
+
+        SetOfSetsOfClasses exclusions =
+                new SetOfSetsOfClasses(SetOfSetsOfClasses.Kind.INCL_OVERRIDE_EXCL);
+        InputStream exclusionStream =
+                new FileInputStream((new FileProvider()).getFile("GUIExclusions.txt"));
+        exclusions.addSet(new FileOfClasses(exclusionStream));
+        Collection<String> inclusions = new HashSet<String>();
+        inclusions.add("java/awt/Button");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions, true, false));
+        scope.setExclusions(exclusions);
+
+        assertFalse(scope.getExclusions().contains(buttonRef.getName().toString().substring(1)));
+    }
+
+    @Test
+    public void testMultipleInclusions() throws IOException {
+        AnalysisScope scope =
+                AnalysisScopeReader.readJavaScope(
+                        TestConstants.WALA_TESTDATA, null, ExclusionsTest.class.getClassLoader());
+        TypeReference buttonRef =
+                TypeReference.findOrCreate(
+                        ClassLoaderReference.Application,
+                        StringStuff.deployment2CanonicalTypeString("java.awt.Button"));
+
+        SetOfSetsOfClasses exclusions = new SetOfSetsOfClasses(SetOfSetsOfClasses.Kind.INCL_ONLY);
+        Collection<String> inclusions1 = new HashSet<String>();
+        inclusions1.add("java/awt/Button");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions1, true, false));
+        Collection<String> inclusions2 = new HashSet<String>();
+        inclusions2.add("java/awt/Canvas");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions2, true, false));
+        scope.setExclusions(exclusions);
+
+        assertFalse(scope.getExclusions().contains(buttonRef.getName().toString().substring(1)));
+    }
+
+    @Test
+    public void testMultipleInclusionsSingleExclusions() throws IOException {
+        AnalysisScope scope =
+                AnalysisScopeReader.readJavaScope(
+                        TestConstants.WALA_TESTDATA, null, ExclusionsTest.class.getClassLoader());
+        TypeReference buttonRef =
+                TypeReference.findOrCreate(
+                        ClassLoaderReference.Application,
+                        StringStuff.deployment2CanonicalTypeString("java.awt.Button"));
+
+        SetOfSetsOfClasses exclusions =
+                new SetOfSetsOfClasses(SetOfSetsOfClasses.Kind.INCL_OVERRIDE_EXCL);
+        InputStream exclusionStream =
+                new FileInputStream((new FileProvider()).getFile("GUIExclusions.txt"));
+        exclusions.addSet(new FileOfClasses(exclusionStream));
+        Collection<String> inclusions1 = new HashSet<String>();
+        inclusions1.add("java/awt/Button");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions1, true, false));
+        Collection<String> inclusions2 = new HashSet<String>();
+        inclusions2.add("java/awt/Canvas");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions2, true, false));
+        scope.setExclusions(exclusions);
+
+        assertFalse(scope.getExclusions().contains(buttonRef.getName().toString().substring(1)));
+    }
+
+    @Test
+    public void testMultipleInclusionsMultipleExclusions() throws IOException {
+        AnalysisScope scope =
+                AnalysisScopeReader.readJavaScope(
+                        TestConstants.WALA_TESTDATA, null, ExclusionsTest.class.getClassLoader());
+        TypeReference buttonRef =
+                TypeReference.findOrCreate(
+                        ClassLoaderReference.Application,
+                        StringStuff.deployment2CanonicalTypeString("java.awt.Button"));
+
+        SetOfSetsOfClasses exclusions =
+                new SetOfSetsOfClasses(SetOfSetsOfClasses.Kind.INCL_OVERRIDE_EXCL);
+        InputStream exclusionStream1 =
+                new FileInputStream((new FileProvider()).getFile("GUIExclusions.txt"));
+        exclusions.addSet(new FileOfClasses(exclusionStream1));
+        InputStream exclusionStream2 =
+                new FileInputStream((new FileProvider()).getFile("GUICorbaExclusions.txt"));
+        exclusions.addSet(new FileOfClasses(exclusionStream2));
+        Collection<String> inclusions1 = new HashSet<String>();
+        inclusions1.add("java/awt/Button");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions1, true, false));
+        Collection<String> inclusions2 = new HashSet<String>();
+        inclusions2.add("java/awt/Canvas");
+        exclusions.addSet(new ExplicitSetOfClasses(inclusions2, true, false));
+        scope.setExclusions(exclusions);
+
+        assertFalse(scope.getExclusions().contains(buttonRef.getName().toString().substring(1)));
+    }
 }
--- com.ibm.wala.util/src/com/ibm/wala/util/config/SetOfSetsOfClasses.java
+++ com.ibm.wala.util/src/com/ibm/wala/util/config/SetOfSetsOfClasses.java
@@ -1,20 +1,49 @@
 package com.ibm.wala.util.config;
 
+import com.ibm.wala.util.debug.UnimplementedError;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import com.ibm.wala.util.debug.UnimplementedError;
-
+/**
+ * Supports the abstraction of multiple sets of classes to allow both exclusions and "negative
+ * exclusions" i.e. inclusions for WALA's AnalysisScope. A notable usage of exclusions is in
+ * com.ibm.wala.classLoader.ClassLoaderImpl.loadAllClasses().
+ *
+ * <p>Currently we support three kinds of SetsOfSetsOfClasses (SOSOC's):
+ *
+ * <ul>
+ * <li>inclusions-only - the SOSOC specifies one or more sets of classes to be included, and no
+ *     exclusion sets. contains(klass) will return true iff klass is in the complement of the union
+ *     of the inclusion sets.
+ * <li>exclusions-only - the SOSOC specifies one or more sets of classes to be excluded, and no
+ *     inclusion sets. contains(klass) will return true iff klass is in the union of the exclusion
+ *     sets.
+ * <li>both inclusions and exclusions, with inclusions taking priority in case of conflicts.
+ *     contains(klass) is true iff klass is in the union of the exclusion sets minus the union of
+ *     the inclusion sets.
+ * </ul>
+ */
 public class SetOfSetsOfClasses extends SetOfClasses implements Serializable {
+
+    public static enum Kind {
+        INCL_ONLY,
+        EXCL_ONLY,
+        INCL_OVERRIDE_EXCL
+    }
+
+    private Kind kind;
 
   private Collection<SetOfClasses> sets = new ArrayList<SetOfClasses>();
   
-  public SetOfSetsOfClasses() {}
-  
-  public SetOfSetsOfClasses(Collection<SetOfClasses> sets) {
-    this.sets.addAll(sets);
-  }
+    public SetOfSetsOfClasses(Kind kind) {
+        this.kind = kind;
+    }
+
+    public SetOfSetsOfClasses(Collection<SetOfClasses> sets, Kind kind) {
+        this.sets.addAll(sets);
+        this.kind = kind;
+    }
   
   public void addSet(SetOfClasses set) {
     this.sets.add(set);
@@ -22,10 +51,27 @@ public class SetOfSetsOfClasses extends SetOfClasses implements Serializable {
   
   @Override
   public boolean contains(String klassName) {
-    for (SetOfClasses set : sets) {
-      if (set.contains(klassName)) return true;
-    }
-    return false;
+        boolean isExcluded = false;
+        boolean isIncluded = false;
+        for (SetOfClasses set : sets) {
+            if ((set instanceof ExplicitSetOfClasses)
+                    && ((ExplicitSetOfClasses) set).isInverted()) { // inclusion
+                if (!set.contains(klassName)) {
+                    isIncluded = true;
+                }
+            } else { // exclusion
+                if (set.contains(klassName)) {
+                    isExcluded = true;
+                }
+            }
+        }
+        if (kind == Kind.INCL_ONLY) {
+            return !isIncluded;
+        } else if (kind == Kind.EXCL_ONLY) {
+            return isExcluded;
+        } else { // kind is INCL_OVERRIDE_EXCL
+            return isExcluded && !isIncluded;
+        }
   }
 
   @Override
